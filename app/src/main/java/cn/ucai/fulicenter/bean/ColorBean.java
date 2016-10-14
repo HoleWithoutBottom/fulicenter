a97123450f491844b2b73cf4e8fefdb6adc09463
沈坤 <980507344@qq.com>
2016-10-14 11:08:53 +0800

    Revert "修改 ColorBean"
    This reverts commit ec44ce064a2762c54ba32a0a75c7dcc8bb23e2bb.

--- app/src/main/java/cn/ucai/fulicenter/bean/ColorBean.java
+++ app/src/main/java/cn/ucai/fulicenter/bean/ColorBean.java
@@ -4,45 +4,4 @@ package cn.ucai.fulicenter.bean;
  * Created by Administrator on 2016/10/13.
  */
 public class ColorBean {
-
-
-    private int colorId;
-    private String colorName;
-    private String colorCode;
-    private String colorUrl;
-
-    public int getColorId() {
-        return colorId;
-    }
-
-    public void setColorId(int colorId) {
-        this.colorId = colorId;
-    }
-
-    public String getColorName() {
-        return colorName;
-    }
-
-    public void setColorName(String colorName) {
-        this.colorName = colorName;
-    }
-
-    public String getColorCode() {
-        return colorCode;
-    }
-
-    public void setColorCode(String colorCode) {
-        this.colorCode = colorCode;
-    }
-
-    public String getColorUrl() {
-        return colorUrl;
-    }
-
-    public void setColorUrl(String colorUrl) {
-        this.colorUrl = colorUrl;
-    }
-
-    public ColorBean() {
-    }
 }
