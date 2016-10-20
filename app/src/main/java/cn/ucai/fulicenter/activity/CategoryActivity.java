db5e234fc82826feee626c64be7c9bd4909e76df
沈坤 <980507344@qq.com>
2016-10-20 14:10:54 +0800

    实现分类二级页面商品按照价格排序，按照时间排序好像所有商品上架时间都一样就无法排序了

--- app/src/main/java/cn/ucai/fulicenter/activity/CategoryActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/CategoryActivity.java
@@ -1,6 +1,7 @@
 package cn.ucai.fulicenter.activity;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -9,6 +10,10 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -19,6 +24,7 @@ import cn.ucai.fulicenter.adapter.NewGoodsAdapter;
 import cn.ucai.fulicenter.bean.NewGoodsBean;
 import cn.ucai.fulicenter.utils.CommonUtils;
 import cn.ucai.fulicenter.utils.L;
+import cn.ucai.fulicenter.utils.MFGT;
 import cn.ucai.fulicenter.utils.OkHttpUtils;
 
 public class CategoryActivity extends AppCompatActivity {
@@ -31,7 +37,7 @@ public class CategoryActivity extends AppCompatActivity {
     TextView tvCategoryPriceOrderUp;
     @Bind(R.id.tv_category_time_orderDown)
     TextView tvCategoryTimeOrderDown;
-
+    int catId = 1;
     ArrayList<NewGoodsBean> mNewGoodsBeanList;
     int mPageID = 1;
     @Bind(R.id.tv_category_refresh)
@@ -40,21 +46,91 @@ public class CategoryActivity extends AppCompatActivity {
     RecyclerView recyclerCategory;
     GridLayoutManager gridLayoutManager;
     NewGoodsAdapter mAdapter;
-
+    @Bind(R.id.swipe_category)
+    SwipeRefreshLayout swipeCategory;
+    Comparator<NewGoodsBean> comparatorPrice;
+    Comparator<NewGoodsBean> comparatorAddTime;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
         ButterKnife.bind(this);
         initData();
+        initAdapter();
+        initComparator();
+        setListener();
+    }
 
+    private void initComparator() {
+        comparatorPrice = new Comparator<NewGoodsBean>() {
+            @Override
+            public int compare(NewGoodsBean newGoodsBean1, NewGoodsBean newGoodsBean2) {
+                String shopPrice2 = newGoodsBean2.getShopPrice();
+                String shopPrice1 = newGoodsBean1.getShopPrice();
+                int i =0;
+                int j =0;
+                Pattern pattern = Pattern.compile("[0-9]");
+                Matcher matcher = pattern.matcher(shopPrice1);
+                if (matcher.find()){
+                    i=shopPrice1.indexOf(matcher.group());
+                }
+                matcher=pattern.matcher(shopPrice2);
+                if (matcher.find()){
+                    j=shopPrice2.indexOf(matcher.group());
+                }
+                return Integer.parseInt(shopPrice1.substring(i)) - Integer.parseInt(shopPrice2.substring(j));
+            }
+        };
+        comparatorAddTime=new Comparator<NewGoodsBean>() {
+            @Override
+            public int compare(NewGoodsBean newGoodsBean1, NewGoodsBean newGoodsBean2) {
+                //L.e(newGoodsBean1.getAddTime()+":"+newGoodsBean2.getAddTime());
+                long l = newGoodsBean2.getAddTime() - newGoodsBean1.getAddTime();
+                return (int)l;
+            }
+        };
+    }
+
+    private void setListener() {
+        gridLayoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+            @Override
+            public int getSpanSize(int position) {
+                return position == mAdapter.getItemCount() - 1 ? 2 : 1;
+            }
+        });
+        swipeCategory.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                swipeCategory.setRefreshing(true);
+                swipeCategory.setEnabled(true);
+                mPageID = 1;
+                downloadNewGoodsBeanList(catId, I.ACTION_PULL_DOWN, mPageID);
+                tvCategoryRefresh.setVisibility(View.VISIBLE);
+            }
+        });
+        recyclerCategory.setOnScrollListener(new RecyclerView.OnScrollListener() {
+            int lastPosition;
+
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                lastPosition = gridLayoutManager.findLastVisibleItemPosition();
+                mAdapter.setmNewState(newState);
+                if (mAdapter.isMore() && newState == recyclerView.SCROLL_STATE_IDLE && mAdapter.getItemCount() - 1 == lastPosition) {
+                    mPageID++;
+                    downloadNewGoodsBeanList(catId, I.ACTION_PULL_UP, mPageID);
+                }
+            }
+        });
     }
 
     private void initData() {
         mNewGoodsBeanList = new ArrayList<>();
-        int catId = getIntent().getIntExtra("goodsId", 1);
+        catId = getIntent().getIntExtra("goodsId", 1);
+        String name = getIntent().getStringExtra("name");
+        tvCategoryTitle.setText(name);
         if (catId != 1) {
-            downloadGoodsDetailsBeanList(catId, I.ACTION_DOWNLOAD, mPageID);
+            downloadNewGoodsBeanList(catId, I.ACTION_DOWNLOAD, mPageID);
         } else {
             CommonUtils.showShortToast("没有该物品");
         }
@@ -67,7 +143,7 @@ public class CategoryActivity extends AppCompatActivity {
         recyclerCategory.setLayoutManager(gridLayoutManager);
     }
 
-    private void downloadGoodsDetailsBeanList(int catId, int action, int pageId) {
+    private void downloadNewGoodsBeanList(int catId, final int action, int pageId) {
         final OkHttpUtils<NewGoodsBean[]> utils = new OkHttpUtils<>(this);
         utils.setRequestUrl(I.REQUEST_FIND_GOODS_DETAILS)
                 .addParam(I.GoodsDetails.KEY_CAT_ID, catId + "")
@@ -79,7 +155,30 @@ public class CategoryActivity extends AppCompatActivity {
                     public void onSuccess(NewGoodsBean[] result) {
                         if (result != null && result.length > 0) {
                             mNewGoodsBeanList = utils.array2List(result);
-                            initAdapter();
+                            mAdapter.setMore(true);
+                            if (action == I.ACTION_PULL_UP) {
+                                if (!mAdapter.isMore()) {
+                                    mAdapter.setFoot("没有多余数据");
+                                }
+                                return;
+                            }
+                            mAdapter.setFoot("下拉加载更多数据");
+                            switch (action) {
+                                case I.ACTION_DOWNLOAD:
+                                    mAdapter.initGoodsList(utils.array2List(result));
+                                    break;
+                                case I.ACTION_PULL_DOWN:
+                                    mAdapter.initGoodsList(utils.array2List(result));
+                                    swipeCategory.setRefreshing(false);
+                                    tvCategoryRefresh.setVisibility(View.GONE);
+                                    break;
+                                case I.ACTION_PULL_UP:
+                                    mAdapter.addGoodsList(utils.array2List(result));
+                                    break;
+                            }
+                        } else {
+                            mAdapter.setMore(false);
+                            mAdapter.setFoot("没有多余数据");
                         }
                     }
 
@@ -95,12 +194,21 @@ public class CategoryActivity extends AppCompatActivity {
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.iv_category_back:
+                MFGT.finish(this);
                 break;
             case R.id.tv_category_title:
+                MFGT.finish(this);
                 break;
             case R.id.tv_category_price_orderUp:
+                Collections.sort(mNewGoodsBeanList, comparatorPrice);
+                mAdapter.initGoodsList(mNewGoodsBeanList);
+                mAdapter.notifyDataSetChanged();
                 break;
             case R.id.tv_category_time_orderDown:
+                L.e("sb");
+                Collections.sort(mNewGoodsBeanList, comparatorAddTime);
+                mAdapter.initGoodsList(mNewGoodsBeanList);
+                mAdapter.notifyDataSetChanged();
                 break;
         }
     }
