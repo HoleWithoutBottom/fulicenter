3b1d69de924e451841ee63c06125beefd405113d
沈坤 <980507344@qq.com>
2016-10-20 19:57:22 +0800

    添加ArrayList<GoodsDetailBean>用于存储点击收藏的商品类

--- app/src/main/java/cn/ucai/fulicenter/FuLiCenterApplication.java
+++ app/src/main/java/cn/ucai/fulicenter/FuLiCenterApplication.java
@@ -2,18 +2,24 @@ package cn.ucai.fulicenter;
 
 import android.app.Application;
 
+import java.util.ArrayList;
+
+import cn.ucai.fulicenter.bean.GoodsDetailBean;
+
 /**
  * Created by Administrator on 2016/10/17.
  */
 public class FuLiCenterApplication extends Application {
     public static FuLiCenterApplication application;
     private static FuLiCenterApplication instance;
+    public static ArrayList<GoodsDetailBean> detailBeenList;
 
     @Override
     public void onCreate() {
         super.onCreate();
         instance = this;
         application = this;
+        detailBeenList = new ArrayList<>();
     }
 
     public static FuLiCenterApplication getInstance() {
