550a9307af2e3ef5dd922600e8c46f07783d1e00
沈坤 <980507344@qq.com>
2016-10-28 15:02:25 +0800

    提交支付成功后，删除购物车选择的商品

--- app/src/main/java/cn/ucai/fulicenter/activity/OrderActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/OrderActivity.java
@@ -20,7 +20,9 @@ import cn.ucai.fulicenter.I;
 import cn.ucai.fulicenter.R;
 import cn.ucai.fulicenter.bean.CartBean;
 import cn.ucai.fulicenter.bean.GoodsDetailBean;
+import cn.ucai.fulicenter.bean.MessageBean;
 import cn.ucai.fulicenter.bean.UserAvatar;
+import cn.ucai.fulicenter.utils.CommonUtils;
 import cn.ucai.fulicenter.utils.L;
 import cn.ucai.fulicenter.utils.MFGT;
 import cn.ucai.fulicenter.utils.OkHttpUtils;
@@ -105,36 +107,66 @@ public class OrderActivity extends AppCompatActivity {
                 MFGT.finish(OrderActivity.this);
                 break;
             case R.id.btn_order_confirm:
+                if (total <= 0) {
+                    CommonUtils.showShortToast("您还没有选择商品");
+                    return;
+                }
                 String receiver = etOrderReceiver.getText().toString().trim();
                 String phone = etOrderPhoneNumber.getText().toString().trim();
                 String city = spinnerOrderAddress.getSelectedItem().toString().trim();
                 String street = etStreetAddress.getText().toString().trim();
-                if (receiver==null || receiver.length()==0){
+                if (receiver == null || receiver.length() == 0) {
                     etOrderReceiver.setError("请填写收货人");
                     etOrderReceiver.requestFocus();
                     return;
                 }
-                if (phone==null||phone.length()==0){
+                if (phone == null || phone.length() == 0) {
                     etOrderPhoneNumber.setError("请填写手机号码");
                     etOrderPhoneNumber.requestFocus();
                     return;
                 }
-                if (!phone.matches("[\\d]{11}")){
+                if (!phone.matches("[\\d]{11}")) {
                     etOrderPhoneNumber.setError("手机号码格式错误");
                     etOrderPhoneNumber.requestFocus();
                     return;
                 }
-                if (street==null||street.length()==0){
+                if (street == null || street.length() == 0) {
                     etStreetAddress.setError("请填写街道地址");
                     etStreetAddress.requestFocus();
                 }
-                if (true){
+                if (true) {
                     // 删除结算的商品
 
-                }else {
+                } else {
 
                 }
                 break;
+        }
+    }
+
+    public void deleteCart() {
+        OkHttpUtils<MessageBean> utils = new OkHttpUtils<>(OrderActivity.this);
+        for (int i = 0; i < cartList.size(); i++) {
+            if (cartList.get(i).isChecked()) {
+                int cartId = cartList.get(i).getId();
+                utils.setRequestUrl(I.REQUEST_DELETE_CART)
+                        .addParam(I.Cart.ID, cartId + "")
+                        .targetClass(MessageBean.class)
+                        .execute(new OkHttpUtils.OnCompleteListener<MessageBean>() {
+                            @Override
+                            public void onSuccess(MessageBean result) {
+                                if (result != null && result.isSuccess()) {
+                                }else {
+                                    CommonUtils.showShortToast("从购物车删除失败");
+                                }
+                            }
+
+                            @Override
+                            public void onError(String error) {
+
+                            }
+                        });
+            }
         }
     }
 }
